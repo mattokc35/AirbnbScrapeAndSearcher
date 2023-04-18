69cb8799aa8e46b902e90e1536b4e1b2724e3759
Matthew Chen <43581549+mattokc35@users.noreply.github.com>
2023-04-18 18:37:15 -0500

    command line arguments + url constructor

--- search.py
+++ search.py
@@ -1,40 +1,15 @@
 import requests
-from scrapy.selector import Selector
 import sys
 import csv
-
+import getopt
+from scrapy.selector import Selector
 from bs4 import BeautifulSoup
 
 
 """
-TODO: write data to .csv/excel file, implement command line inputs
+TODO: export listing name, start external script
 
 """
-
-# (temporary) input your Airbnb search url here
-airbnb_url = ""
-# (temporary) put the name of your listing here
-my_beachhouse = ""
-listingsTotal = []
-pageNumber = 1
-
-# if listings is found
-listingFound = False
-
-# if you want all listings
-allListings = False
-
-# file
-f = open("dataFile.txt", "a", encoding="utf-8")
-
-# csv file
-csvFile = open('dataFile.csv', 'w', encoding='UTF8')
-writer = csv.writer(csvFile)
-
-# csv header
-header = ['Listing Card Title', 'Listing Card Subtitle', 'Beds', 'Bedrooms', 'Current Nightly Price', 'Previous Nightly Price', 'Total Price', 'Superhost/Rare Find', 'Ratings/Reviews']
-writer.writerow(header)
-
 
 
 """
@@ -95,12 +70,10 @@ def get_listings(search_page, page_number):
         else:
             super_host = listings[i].find(
                 'div', {'class': 't1mwk1n0 dir dir-ltr'}).text
-        print(ratings_reviews)
-        print(super_host)
 
-        # write to file
+        # write to txt file
         f.write("NEXT LISTING " + "\n\n\n")
-        #f.write(listings[i].prettify() + '\n\n\n')
+        # f.write(listings[i].prettify() + '\n\n\n')
         f.write(listing_card_name + '\n')
         f.write(listing_card_subtitle + '\n')
         f.write(num_beds + '\n')
@@ -111,10 +84,11 @@ def get_listings(search_page, page_number):
         f.write("Superhost/Rare Find: " + super_host + '\n')
         f.write(ratings_reviews + '\n\n\n')
 
-        # write row to csv
-        writer.writerow([listing_card_name, listing_card_subtitle, num_beds, num_bedrooms, current_price, previous_price, total_price, super_host, ratings_reviews])
+        # write row to csv file
+        writer.writerow([listing_card_name, listing_card_subtitle, num_beds, num_bedrooms,
+                        current_price, previous_price, total_price, super_host, ratings_reviews])
 
-        # my beachhouse is founds
+        # if my beachhouse is found
         if my_beachhouse in listings[i].get_text():
             found = True
             break
@@ -136,6 +110,83 @@ def listing_found(page_number):
         sys.exit()
 
 
+"""
+Builds Airbnb search url based on user input parameters
+"""
+
+
+def url_constructor(location, checkin, checkout, adults, children, infants, pets):
+    return "https://www.airbnb.com/s/" + location + "/homes?tab_id=home_tab&refinement_paths%5B%5D=%2Fhomes&date_picker_type=calendar&checkin=" + checkin + "&checkout=" + checkout + "&adults=" + adults + "&children=" + children + "&infants=" + infants + "&pets=" + pets + "&source=structured_search_input_header&search_type=filter_change"
+
+
+"""
+Parses command line arguments (user input)
+"""
+
+
+def command_line_arguments():
+    opts, args = getopt.getopt(sys.argv[1:], "hlcoa:kip",  ["location=", "checkin=", "checkout=",
+                                                            "adults=", "children=", "infants=", "pets="])
+    # checking each argument
+    for opt, arg in opts:
+        if opt == '-h':
+            print("Example Arguments:")
+            print('--location=<Your-Location> --checkin=2023-01-01 --checkout=2023-01-01 --adults=10 --children=1 --infants=1 --pets=1')
+            sys.exit()
+        elif opt in ("-l", "--location"):
+            location = arg
+        elif opt in ("-c", "--checkin"):
+            checkin = arg
+        elif opt in ("-o", "--checkout"):
+            checkout = arg
+        elif opt in ("-a", "--adults"):
+            adults = arg
+        elif opt in ("-k", "--children"):
+            children = arg
+        elif opt in ("-i", "--infants"):
+            infants = arg
+        elif opt in ("-p", "--pets"):
+            pets = arg
+    return location, checkin, checkout, adults, children, infants, pets
+
+
+# these optional arguments start with a default value
+children = "0"
+infants = "0"
+pets = "0"
+
+# initialize command line arguments
+location, checkin, checkout, adults, children, infants, pets = command_line_arguments()
+
+# contructs the Airbnb Search URL based on command line arguments
+airbnb_url = url_constructor(
+    location, checkin, checkout, adults, children, infants, pets)
+print(airbnb_url)
+
+# (temporary) put the name of your listing here
+my_beachhouse = ""
+listingsTotal = []
+pageNumber = 1
+
+# if listings is found
+listingFound = False
+
+# if you want all listings
+allListings = False
+
+# file
+f = open("dataFile.txt", "a", encoding="utf-8")
+
+# csv file
+csvFile = open('dataFile.csv', 'w', encoding='UTF8')
+writer = csv.writer(csvFile, lineterminator='\n')
+
+# csv header
+header = ['Listing Card Title', 'Listing Card Subtitle', 'Beds', 'Bedrooms', 'Current Nightly Price',
+          'Previous Nightly Price', 'Total Price', 'Superhost/Rare Find', 'Ratings/Reviews']
+writer.writerow(header)
+
+# get listings for first page
 listings, next_page_url, found, page_number = get_listings(airbnb_url, 1)
 
 print(str(found))
@@ -143,7 +194,7 @@ if found == True:
     listing_found(page_number)
 
 
-# after the first page we will go through each page and add listings to listingsTotal
+# after the first page, we will loop through the rest of the pages
 counter = 0
 while next_page_url != None and counter <= 14:
     counter += 1
@@ -156,3 +207,4 @@ while next_page_url != None and counter <= 14:
 
 
 f.close()
+csvFile.close()
